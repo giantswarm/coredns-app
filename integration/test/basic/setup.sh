2ab051318fa8b397e69f2df9474145c1731c0301
ericgraf <eric@giantswarm.io>
2020-12-01 15:49:03 +0000

    Remove dns patch for chart-operator

--- integration/test/basic/setup.sh
+++ integration/test/basic/setup.sh
@@ -10,6 +10,5 @@ kubectl delete serviceaccount coredns -n kube-system
 kubectl delete clusterrole system:coredns
 
 # For app-operator to find the catalog file we need to enable some form of dns
-kubectl patch deployment -n giantswarm chart-operator-unique --patch "$(cat ./integration/test/basic/dns-patch.yaml)"
 kubectl patch deployment -n giantswarm app-operator-unique --patch "$(cat ./integration/test/basic/dns-patch.yaml)"
 
