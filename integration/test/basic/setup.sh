843b2e3d068805db2fd4c7e22632a09ae197f475
ericgraf <eric@giantswarm.io>
2020-12-01 14:30:26 +0000

    update dns for app and chart to see internal dns dependency

--- integration/test/basic/setup.sh
+++ integration/test/basic/setup.sh
@@ -11,4 +11,5 @@ kubectl delete clusterrole system:coredns
 
 # For app-operator to find the catalog file we need to enable some form of dns
 kubectl patch deployment -n giantswarm chart-operator-unique --patch "$(cat ./integration/test/basic/dns-patch.yaml)"
+kubectl patch deployment -n giantswarm app-operator-unique --patch "$(cat ./integration/test/basic/dns-patch.yaml)"
 
